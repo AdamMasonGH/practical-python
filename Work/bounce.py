285f85fb09ce15af6e824b20efe6c4add029bd63
AdamMasonGH <88633451+AdamMasonGH@users.noreply.github.com>
2023-02-27 19:16:26 +0000

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+drop_height = 100
+heights_after_bounce = []
+for i in range(10):
+    drop_height *= 0.6
+    heights_after_bounce.append(round(drop_height, ndigits = 2))
+
+for i, height in enumerate(heights_after_bounce):
+    print(i, height)
